405ad7e69561aa738f0c9c85f9eb25ec46fc2357
NathanDestrebecq <la238254@student.helha.be>
2025-04-15 11:01:41 +0200

    modification dive fonction

--- Assets/Code/Scripts/InputInteractions.cs
+++ Assets/Code/Scripts/InputInteractions.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using PathCreation.Examples;
 using UnityEngine;
 
@@ -30,5 +31,21 @@ public class InputInteractions : MonoBehaviour
         PathFollower pathFollower = vehicle.GetComponent<PathFollower>();
         float tempOffset = pathFollower.offset + pathFollower.widthOffset;
         pathFollower.offset = Mathf.Min(tempOffset, pathFollower.widthOffset);
+    }
+
+    public void Down()
+    {
+        PathFollower pathFollower = vehicle.GetComponent<PathFollower>();
+        float tempHeightOffset = pathFollower.heightOffset - pathFollower.widthOffset;
+        pathFollower.heightOffset = Mathf.Max(tempHeightOffset, -pathFollower.widthOffset);
+        Invoke("Up",2);
+
+    }
+
+    public void Up()
+    {
+        PathFollower pathFollower = vehicle.GetComponent<PathFollower>();
+        float tempHeightOffset = pathFollower.heightOffset + pathFollower.widthOffset;
+        pathFollower.heightOffset = Mathf.Min(tempHeightOffset, pathFollower.widthOffset);
     }
 }
