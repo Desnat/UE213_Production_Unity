b501b0ebc5ca9d19513da590c3d0255192a964d1
la239778-Chelius <122162863+HChelius@users.noreply.github.com>
2025-04-18 02:00:01 +0200

    Update Menu UI
    Add Fonctionnal Pause menu with retry
    Add Fonctionnal Main menu

--- Assets/Code/Scripts/UiMenuPrincipal.cs
+++ Assets/Code/Scripts/UiMenuPrincipal.cs
@@ -1,17 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UiMenuPrincipal : MonoBehaviour
 {
-    
-    
-    
+
     public void RetryLevel(string sceneName)
     {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
         SceneManager.LoadScene(sceneName);
     }
     public void Quit()
     {
         Application.Quit();
+    }
+    public void PlayGame ()
+    {
+	    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
